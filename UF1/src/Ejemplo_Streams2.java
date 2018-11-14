e7e939a332ef726d0888c3ee22e6b00bb5f7cc87
marcfs31 <marcfs31@protonmail.com>
2018-11-14 20:54:41 +0100

    Problema 2 Streams2

---
+++ UF1/src/Ejemplo_Streams2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Ejemplo_Streams2 {
+	public static void main (String [] args) throws IOException {
+		File fichero = new File("FicheroTexto.txt");
+		FileWriter fic = new FileWriter (fichero);
+		String cadena = " Esto es una prueba con FileWriter";
+		char [] cad = cadena.toCharArray () ;
+		for ( int i=0; i < cad.length ; i++) {
+			fic.write (cad[i]); // se va escribiendo carácter a carácter
+			fic.append ("*"); // añadimos un asterisco al final
+		}
+		fic.close (); // cerramos fichero
+	}
+}
