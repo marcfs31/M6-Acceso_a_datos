96649eadf25b279b8c4d22d302feed658c2d3a54
Marc <Marc@Windows-Marc>
2019-02-07 22:18:58 +0100

    Añadido proyecto de Hibernate funcionando

---
+++ Hibernate/src/Main.java
@@ -0,0 +1,27 @@
+import primero.*;
+import primero.HibernateUtil;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class Main {
+	public static void main(String[] args) {
+		//En primer lugar se obtiene la sesión creada por el Singleton.
+		SessionFactory sesion = HibernateUtil.getSessionFactory();
+		//Abrimos sesión e iniciamos una transacción
+		Session session = sesion.openSession();
+		Transaction tx = session.beginTransaction();
+		System.out.println("Inserto una fila en depart");
+		//Creamos un nuevo objeto Depart y damos valor a sus atributos
+		Depart dep = new Depart();
+		dep.setDeptNo(62);
+		dep.setDnombre("MARKETING");
+		dep.setLoc("GUADALAJARA");
+		//Guardamos en la base de datos y comprometemos la información
+		session.save(dep);
+		tx.commit();
+		session.close();
+		System.exit(0);
+	}
+}
